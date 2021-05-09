cbcd3c8d55002900af1531c29094a1cd479b2664
Christophe Lavergne <chlavergne@outlook.fr>
2021-05-09 22:59:30 +0200

    select image work for one image

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var buttonWideTop: UIButton!
     @IBOutlet weak var buttonWideBottom: UIButton!
     @IBOutlet weak var buttonFourSquare: UIButton!
     @IBOutlet weak var frameView: FrameView!
-//   test
+    
     @IBAction func tapButtonWideTop(_ sender: UIButton) {
         frameView.style = .wideTop
         buttonWideTop.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
@@ -32,7 +33,29 @@ class ViewController: UIViewController {
         buttonWideBottom.setImage(nil, for: .normal)
         buttonFourSquare.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
     }
+    @IBAction func chooseImage1(_ sender: UIButton) {
+        openGallery()
+    }
     
+    private func openGallery() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        button1.setBackgroundImage(image, for: .normal)
+        button1.layoutIfNeeded()
+        button1.subviews.first?.contentMode = .scaleAspectFill
+        button1.setImage(nil, for: .normal)
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
